aa235d3e76d34a6cfe52625f5d087606720cfeb1
Mateusz Szostok <szostok.mateusz@gmail.com>
2022-07-23 18:30:18 +0200

    Update codeowners-validator.rb

--- codeowners-validator.rb
+++ codeowners-validator.rb
@@ -10,7 +10,7 @@ class CodeownersValidator < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/mszostok/codeowners-validator/releases/download/v0.7.5-rc1/codeowners-validator_0.7.5-rc1_Darwin_x86_64.tar.gz"
+      url "https://github.com/mszostok/codeowners-validator/releases/download/v0.7.4/codeowners-validator_0.7.4_Darwin_x86_64.tar.gz"
       sha256 "2372dd11f0b34bfa57a956b99daadbed658fb6b45433df95a90d6569b0fa854b"
 
       def install
@@ -18,7 +18,7 @@ class CodeownersValidator < Formula
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/mszostok/codeowners-validator/releases/download/v0.7.5-rc1/codeowners-validator_0.7.5-rc1_Darwin_arm64.tar.gz"
+      url "https://github.com/mszostok/codeowners-validator/releases/download/v0.7.4/codeowners-validator_0.7.4_Darwin_arm64.tar.gz"
       sha256 "a381f961fbc863e851d173b8674a419e6627a3baab9cc2aefa4148eacf8794e4"
 
       def install
@@ -29,7 +29,7 @@ class CodeownersValidator < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/mszostok/codeowners-validator/releases/download/v0.7.5-rc1/codeowners-validator_0.7.5-rc1_Linux_x86_64.tar.gz"
+      url "https://github.com/mszostok/codeowners-validator/releases/download/v0.7.4/codeowners-validator_0.7.4_Linux_x86_64.tar.gz"
       sha256 "cd1733c45b304eaeb5cd09dba9a123a6f9405d2d6acf0d5f6839abf9ad9dcf4d"
 
       def install
@@ -37,7 +37,7 @@ class CodeownersValidator < Formula
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/mszostok/codeowners-validator/releases/download/v0.7.5-rc1/codeowners-validator_0.7.5-rc1_Linux_arm64.tar.gz"
+      url "https://github.com/mszostok/codeowners-validator/releases/download/v0.7.4/codeowners-validator_0.7.4_Linux_arm64.tar.gz"
       sha256 "70256dfb71045027487e6573479da40c6f1a876e95402cf81717e73e140c4c77"
 
       def install
