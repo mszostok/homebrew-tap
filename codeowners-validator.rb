afa87e8dc8fe5a0b79eed23df96b9445c120263a
Mateusz Szostok <szostok.mateusz@gmail.com>
2022-07-23 18:32:11 +0200

    Update codeowners-validator.rb

--- codeowners-validator.rb
+++ codeowners-validator.rb
@@ -11,7 +11,7 @@ class CodeownersValidator < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/mszostok/codeowners-validator/releases/download/v0.7.4/codeowners-validator_0.7.4_Darwin_x86_64.tar.gz"
-      sha256 "2372dd11f0b34bfa57a956b99daadbed658fb6b45433df95a90d6569b0fa854b"
+      sha256 "9170d4bcea3d638a02e198bda01be2faa549ceeafe4deaec94f90ea901bc7b68"
 
       def install
         bin.install "codeowners-validator"
@@ -19,7 +19,7 @@ class CodeownersValidator < Formula
     end
     if Hardware::CPU.arm?
       url "https://github.com/mszostok/codeowners-validator/releases/download/v0.7.4/codeowners-validator_0.7.4_Darwin_arm64.tar.gz"
-      sha256 "a381f961fbc863e851d173b8674a419e6627a3baab9cc2aefa4148eacf8794e4"
+      sha256 "589295a9c012e2325dbf237730709bedae2237b7c46d7eaf1e13095c1bb84c26"
 
       def install
         bin.install "codeowners-validator"
@@ -30,7 +30,7 @@ class CodeownersValidator < Formula
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/mszostok/codeowners-validator/releases/download/v0.7.4/codeowners-validator_0.7.4_Linux_x86_64.tar.gz"
-      sha256 "cd1733c45b304eaeb5cd09dba9a123a6f9405d2d6acf0d5f6839abf9ad9dcf4d"
+      sha256 "334a74bf2b21ba498d90fcb7b0dced8df33ebc2467b4eaa360db0ba8bfc76338"
 
       def install
         bin.install "codeowners-validator"
@@ -38,7 +38,7 @@ class CodeownersValidator < Formula
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/mszostok/codeowners-validator/releases/download/v0.7.4/codeowners-validator_0.7.4_Linux_arm64.tar.gz"
-      sha256 "70256dfb71045027487e6573479da40c6f1a876e95402cf81717e73e140c4c77"
+      sha256 "d2399b8eee6cb8e82635cd21eb8989fb9fcd3c29b25c78edbc810b7cea16ead0"
 
       def install
         bin.install "codeowners-validator"
